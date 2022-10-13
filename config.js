da39097e37a030c61f1df062614b536217eb55b3
ErickEvil15 <jdiazj2100tmp@alumnoguinda.mx>
2022-10-13 06:40:01 -0500

    primer commit

---
+++ config.js
@@ -0,0 +1,12 @@
+export const PORT = process.env.PORT || 3000
+
+export const DB_HOST = process.nextTick.DB_HOST || 'localhost'
+
+export const DB_USER = process.nextTick.DB_USER || 'root'
+
+export const DB_PASSWORD = process.nextTick.DB_PASSWORD || 'n0m3l0'
+
+export const DB_NAME = process.nextTick.DB_NAME || 'comida'
+
+export const DB_PORT = process.nextTick.DB_PORT || 3306
+
